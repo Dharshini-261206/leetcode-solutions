7daf4ece2694cac877ff5714c9b670d54f4ca872
Dharshini-261206 <24149008@drngpit.ac.in>
2026-02-08 20:04:19 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,12 @@
+// Last updated: 08/02/2026, 20:04:18
+class Solution {
+    public boolean canWinNim(int n) {
+        
+        if(n%4==0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
